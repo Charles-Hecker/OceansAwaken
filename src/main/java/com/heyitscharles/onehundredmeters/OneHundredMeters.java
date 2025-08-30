375564d6404156f298e8a183f8bfb04e0f33479c
Charles-M-Hecker <HeckerCh25@spaldinghs.org>
2025-08-30 14:25:48 -0400

    update

--- src/main/java/com/heyitscharles/onehundredmeters/OneHundredMeters.java
+++ src/main/java/com/heyitscharles/onehundredmeters/OneHundredMeters.java
@@ -1,6 +1,8 @@
 package com.heyitscharles.onehundredmeters;
 
+import com.heyitscharles.onehundredmeters.item.tanks;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -33,6 +35,10 @@ public class OneHundredMeters
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        tanks.register(modEventBus);
+
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -48,7 +54,9 @@ public class OneHundredMeters
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(tanks.STANDARD);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
